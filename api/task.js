398885d962f5bbeaa437db89b66960e7b76eef2d
avitalpas <avitalpas@outlook.com>
2024-05-24 16:01:19 +0300

    2

---
+++ api/task.js
@@ -0,0 +1,67 @@
+import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  UpdateCommand,
+  PutCommand,
+  DynamoDBDocumentClient,
+  ScanCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+import crypto from "crypto"
+
+const client = new DynamoDBClient( {region:"us-west-1"});
+const docCliennt = DynamoDBDocumentClient.from(client)
+
+// FETCH
+export const fetchTasks = async () => {
+    const command = new ScanCommand({
+        ExpressionAttributeNames:{ "#name": "name"},
+        ProjectionExpression: "id, #name, completed",
+        TableName: "Tasks",
+    });
+
+    const response = await docClient.send(command);
+    return response;
+};
+
+// CREATE
+export const createTasks = async (name, completed) => {
+    const uuid = crypto.randomUUID()
+
+    const command = new PutCommand({
+        TableName: "Tasks",
+        Item: {
+            id: uuid,
+            name,
+            completed
+        }
+    })
+
+    const response = await docClient.send(command);
+    return response;
+};
+
+// UPDATE
+export const updateTasks = async (id, name, completed) => {
+    const command = new UpdateCommand({
+        TableName: "Tasks",
+        Key:{id},
+        ExpressionAttributeNames:{ "#name": "name"},
+        UpdateExpression: "set #name :n, completed :c",
+        ExpressionAttributeValues: {
+            ":n": name,
+            ":c": completed
+        },
+        ReturnValues: "ALL_NEW"        
+    })
+    
+    const response = await docClient.send(command);
+    return response;
+};
+
+// DELETE
+export const deleteTasks = async (id) => {
+    const command = new DeleteCommand({
+        TableName: "Tasks",
+        Key:{id}
+    })
+};
